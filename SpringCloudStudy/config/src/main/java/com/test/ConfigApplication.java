4d8d8fc7a92b96afa9fbc6b69b38f8555f8ea514
656322295@qq.com <wangheng@appiron.cn>
2022-11-14 13:56:27 +0800

    Config 配置中心 (本地运行未成功显示页面)

---
+++ SpringCloudStudy/config/src/main/java/com/test/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Auther wh
+ * @Date 2022/11/14 10:59
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
